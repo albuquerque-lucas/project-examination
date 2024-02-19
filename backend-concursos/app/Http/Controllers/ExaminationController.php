6b917c09d1dfc6ea366f8d284cd56a7a3ee2b902
albuquerque-lucas <lucaslpra@gmail.com>
2024-02-19 14:00:19 -0300

    Iniciando criacao da rota GET /examinations.Criando controler e metodo index.

---
+++ backend-concursos/app/Http/Controllers/ExaminationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExaminationController extends Controller
+{
+    public function index()
+    {
+        echo "ola Mundo";
+    }
+}
