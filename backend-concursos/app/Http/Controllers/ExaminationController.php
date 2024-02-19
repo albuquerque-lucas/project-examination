339658fbd8d2e8a35c0618fd72509605e4dd2ab9
albuquerque-lucas <lucaslpra@gmail.com>
2024-02-19 15:12:08 -0300

    Criando migration e seeder examinations.

--- backend-concursos/app/Http/Controllers/ExaminationController.php
+++ backend-concursos/app/Http/Controllers/ExaminationController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\ExaminationService;
 
 class ExaminationController extends Controller
 {
+    protected $examinationService;
+
+    public function __construct(ExaminationService $examinationService)
+    {
+        $this->examinationService = $examinationService;
+    }
+
     public function index()
     {
-        echo "ola Mundo";
+        $this->examinationService->getAll();
     }
 }
---
+++ backend-concursos/app/Services/ExaminationService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class ExaminationService
+{
+    public function getAll()
+    {
+        echo "Ola mundo Service.";
+    }
+}
