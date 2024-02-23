4a0fd9934a8cd62d5839a55fb6d256481f576e09
albuquerque-lucas <lucaslpra@gmail.com>
2024-02-23 11:10:48 -0300

    Refatorando metodos de service para melhoria de performance.

--- backend-concursos/app/Services/ExaminationService.php
+++ backend-concursos/app/Services/ExaminationService.php
@@ -19,101 +19,66 @@ class ExaminationService
 
     public function getAll(string $order): ServiceResponse
     {
-        try {
-            // Validar o parâmetro de ordenação
-            if (!in_array($order, ['asc', 'desc'])) {
-                throw new InvalidArgumentException('Parâmetro de ordenação inválido. Use "asc" ou "desc".');
-            }
-
-            $query = Examination::query();
-            $query->orderBy('id', $order);
-
-            $examinations = $query->paginate();
-            $this->serviceResponse->setAttributes(200, $examinations);
-            return $this->serviceResponse;
-        } catch (Exception $exception) {
-            $message =  $exception->getMessage();
-            $this->serviceResponse->setAttributes(500, (object)['Ocorreu um erro' => $message]);
-            return $this->serviceResponse;
-        }
+        return $this->executeSearch(null, $order);
     }
 
     public function getById(int $id): ServiceResponse
     {
-        try {
-            $examination = Examination::query()
-                ->where('id', $id)
-                ->firstOrFail();
-            
-            $this->serviceResponse->setAttributes(200, $examination);
-            return $this->serviceResponse;
-        } catch(Exception $exception)
-        {
-            $message =  $exception->getMessage();
-            $this->serviceResponse->setAttributes(500, (object)['Ocorreu um erro' => $message]);
-            return $this->serviceResponse;
-        }
+        return $this->executeSearch(['id' => $id], null);
     }
 
     public function getByTitle(string $title, string $order): ServiceResponse
     {
-        try {
-            if (!in_array($order, ['asc', 'desc'])) {
-                throw new InvalidArgumentException('Parâmetro de ordenação inválido. Use "asc" ou "desc".');
-            }
-
-            $query = Examination::query();
-            $query->orderBy('id', $order)
-                ->where('title', 'like', "%{$title}%");
-
-            $examinations = $query->paginate();
-            $this->serviceResponse->setAttributes(200, $examinations);
-            return $this->serviceResponse;
-        } catch (Exception $exception) {
-            $message =  $exception->getMessage();
-            $this->serviceResponse->setAttributes(500, (object)['Ocorreu um erro' => $message]);
-            return $this->serviceResponse;
-        }
+        return $this->executeSearch(['title' => "%{$title}%"], $order, 'like');
     }
 
     public function getByInstitution(string $institution, string $order): ServiceResponse
     {
-        try {
-            if (!in_array($order, ['asc', 'desc'])) {
-                throw new InvalidArgumentException('Parâmetro de ordenação inválido. Use "asc" ou "desc".');
-            }
-
-            $query = Examination::query();
-            $query->orderBy('id', $order)
-                ->where('institution', 'like', "%{$institution}%");
-
-            $examinations = $query->paginate();
-            $this->serviceResponse->setAttributes(200, $examinations);
-            return $this->serviceResponse;
-        } catch (Exception $exception) {
-            $message =  $exception->getMessage();
-            $this->serviceResponse->setAttributes(500, (object)['Ocorreu um erro' => $message]);
-            return $this->serviceResponse;
-        }
+        return $this->executeSearch(['institution' => "%{$institution}%"], $order, 'like');
     }
 
     public function getByExamDate(string $examDate, string $order): ServiceResponse
     {
+        $parsedDate = DateTime::createFromFormat('Y-m-d', $examDate);
+        if (!$parsedDate) {
+            throw new InvalidArgumentException('Data inválida. Use o formato YYYY-MM-DD.');
+        }
+
+        return $this->executeSearch(['exam_date' => $parsedDate->format('Y-m-d')], $order);
+    }
+
+    private function executeSearch(array $conditions = null, string $order = null, string $searchType = '='): ServiceResponse
+    {
         try {
-            if (!in_array($order, ['asc', 'desc'])) {
+            // $startTime = microtime(true);
+
+            if ($order && !in_array($order, ['asc', 'desc'])) {
                 throw new InvalidArgumentException('Parâmetro de ordenação inválido. Use "asc" ou "desc".');
             }
-    
-            $parsedDate = DateTime::createFromFormat('Y-m-d', $examDate);
-            if (!$parsedDate) {
-                throw new InvalidArgumentException('Data inválida. Use o formato YYYY-MM-DD.');
-            }
-    
+
             $query = Examination::query();
-            $query->orderBy('id', $order)
-                ->whereDate('exam_date', $parsedDate->format('Y-m-d'));
-    
+
+            if ($conditions) {
+                foreach ($conditions as $field => $value) {
+                    if ($searchType === 'like') {
+                        $query->where($field, $searchType, $value);
+                    } else {
+                        $query->where($field, $value);
+                    }
+                }
+            }
+
+            if ($order) {
+                $query->orderBy('id', $order);
+            }
+
             $examinations = $query->paginate();
+
+            // $endTime = microtime(true);
+            // $executionTime = $endTime - $startTime;
+        
+            // echo "Tempo de execução: {$executionTime} segundos";
+
             $this->serviceResponse->setAttributes(200, $examinations);
             return $this->serviceResponse;
         } catch (Exception $exception) {
