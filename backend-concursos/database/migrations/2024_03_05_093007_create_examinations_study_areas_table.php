9d82c4aa522c0ffb43e3cc3bfc3828ea818a3654
albuquerque-lucas <lucaslpra@gmail.com>
2024-03-05 13:11:49 -0300

    Implementando as migrations de relacionamento many to many

---
+++ backend-concursos/database/migrations/2024_03_05_093007_create_examinations_study_areas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Examination;
+use App\Models\StudyArea;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('examinations_study_areas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Examination::class);
+            $table->foreignIdFor(StudyArea::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('examinations_study_areas');
+    }
+};
