46e0e322fbb57d3ff43cbaa776691d91e219ab72
albuquerque-lucas <lucaslpra@gmail.com>
2024-03-06 14:42:31 -0300

    Implementando a logica em NoticeController.

--- backend-concursos/app/Http/Controllers/NoticeController.php
+++ backend-concursos/app/Http/Controllers/NoticeController.php
@@ -25,26 +25,7 @@ class NoticeController extends Controller
 
     public function getAll(Request $request)
     {
-        try {
-            $order = $request->input('order', 'desc');
-            $response = $this->noticeService->getAll($order);
-            $data = $response->data();
-            $dataArray = (array)$data;
-            if (array_key_exists('code', $dataArray)) {
-                if ($dataArray['code'] === 204) {
-                    return response()->noContent();
-                }
-            }
-            return response()->json($dataArray['resource'], $response->status());
-        } catch (Exception $exception) {
-            return response()->json(['message' => $exception->getMessage(), 'code' => $exception->getCode()], 500);
-        } catch (Error $error) {
-            return response()->json([
-                'error' => 'Ocorreu um erro inesperado.',
-                'message' => $error->getMessage(),
-                'code' => $error->getCode()
-        ], 500);
-        }
+        $this->dataRetrievalService->getAll($this->noticeService, $request);
     }
 
     public function getById(int $id)
