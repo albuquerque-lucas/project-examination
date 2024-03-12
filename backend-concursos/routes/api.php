e5edadcf65aeed23fdde134c317cda4a6aa810bc
albuquerque-lucas <lucaslpra@gmail.com>
2024-03-12 14:16:18 -0300

    Modificando metodo getByName de AccountPlan.

--- backend-concursos/routes/api.php
+++ backend-concursos/routes/api.php
@@ -27,38 +27,22 @@ use App\Http\Controllers\AccountPlanController;
 Route::get('/users/all', [UserController::class, "getAll"]);
 
 // ROTAS DE EXAMINATIONS
-Route::get("/examinations/all", [ExaminationController::class, 'getAll'])
-->middleware(ValidateOrderParam::class);
-
-
+Route::get("/examinations/all", [ExaminationController::class, 'getAll'])->middleware(ValidateOrderParam::class);
+Route::get('/examinations/id/{id}', [ExaminationController::class, "getById"]);
 Route::post("/create/examination", [ExaminationController::class, "create"]);
-
 Route::patch("/examinations/update/{id}", [ExaminationController::class, "update"]);
-
 Route::delete("/examinations/delete/{id}", [ExaminationController::class, "delete"]);
+Route::get('/examinations/registration-date', [ExaminationController::class, "getByRegistrationDate"])->middleware(ValidateOrderParam::class);
+Route::get('/examinations/institution', [ExaminationController::class, "getByInstitution"])->middleware(ValidateOrderParam::class);
+Route::get('/examinations/title', [ExaminationController::class, "getByTitle"])->middleware(ValidateOrderParam::class);
+Route::get('/examinations/educational-level/{id}', [ExaminationController::class, 'getByEducationalLevel'])->middleware(ValidateOrderParam::class);
+Route::get('/examinations/activity-status', [ExaminationController::class, 'getByActivityStatus'])->middleware(ValidateOrderParam::class);
 
-Route::get('/examinations/registration-date', [ExaminationController::class, "getByRegistrationDate"])
-->middleware(ValidateOrderParam::class);
-
-Route::get('/examinations/institution', [ExaminationController::class, "getByInstitution"])
-->middleware(ValidateOrderParam::class);
-
-Route::get('/examinations/title', [ExaminationController::class, "getByTitle"])
-->middleware(ValidateOrderParam::class);
-
-Route::get('/examinations/educational-level/{id}', [ExaminationController::class, 'getByEducationalLevel'])
-->middleware(ValidateOrderParam::class);
-
-Route::get('/examinations/activity-status', [ExaminationController::class, 'getByActivityStatus'])
-->middleware(ValidateOrderParam::class);
-
-Route::get('/examinations/{id}', [ExaminationController::class, "getById"]);
 
   // ROTAS DE NOTICES
-  Route::get('/notices/all', [NoticeController::class, 'getAll'])
-  ->middleware(ValidateOrderParam::class);
+  Route::get('/notices/all', [NoticeController::class, 'getAll'])->middleware(ValidateOrderParam::class);
   Route::post('/notices/create', [NoticeController::class, 'create']);
-  Route::get('/notices/{id}', [NoticeController::class, 'getById']);
+  Route::get('/notices/id/{id}', [NoticeController::class, 'getById']);
   Route::patch('/notices/update/{id}', [NoticeController::class, 'update']);
   Route::delete('/notices/delete/{id}', [NoticeController::class, 'delete']);
   Route::delete('/notices/delete/examination/{id}', [NoticeController::class, 'deleteByExamination']);
@@ -68,7 +52,7 @@ Route::get('/examinations/{id}', [ExaminationController::class, "getById"]);
   Route::get('/subjects/all', [SubjectController::class, 'getAll'])
   ->middleware(ValidateOrderParam::class);;
   Route::get('/subjects/title', [SubjectController::class, 'getByTitle']);
-  Route::get('/subjects/{id}', [SubjectController::class, 'getById']);
+  Route::get('/subjects/id/{id}', [SubjectController::class, 'getById']);
   Route::post('/subjects/create', [SubjectController::class, 'create']);
   Route::patch('/subjects/update/{id}', [SubjectController::class, 'update']);
   Route::delete('/subjects/delete/{id}', [SubjectController::class, 'delete']);
@@ -76,7 +60,7 @@ Route::get('/examinations/{id}', [ExaminationController::class, "getById"]);
 
   // ROTAS DE STUDY AREAS
   Route::get('/study-areas/all', [StudyAreaController::class, 'getAll']);
-  Route::get('/study-areas/{id}', [StudyAreaController::class, 'getById']);
+  Route::get('/study-areas/id/{id}', [StudyAreaController::class, 'getById']);
   Route::get('/study-areas/area', [StudyAreaController::class, 'getByArea']);
   Route::post('/study-areas/create', [StudyAreaController::class, 'create']);
   Route::patch('/study-areas/update/{id}', [StudyAreaController::class, 'update']);
@@ -85,25 +69,28 @@ Route::get('/examinations/{id}', [ExaminationController::class, "getById"]);
 
 // ROTAS DE TOPIC
 Route::get('/topics/all', [TopicController::class,'getAll']);
-Route::get('/topics/{id}', [TopicController::class,'getById']);
+Route::get('/topics/id/{id}', [TopicController::class,'getById']);
 Route::post('/topics/create', [TopicController::class,'create']);
 Route::patch('/topics/update/{id}', [TopicController::class,'update']);
 Route::delete('/topics/delete/{id}', [TopicController::class,'delete']);
 
+
 // ROTAS DE EXAMS
 Route::get('/exams/all', [ExamController::class,'getAll']);
-Route::get('/exams/{id}', [ExamController::class,'getById']);
+Route::get('/exams/id/{id}', [ExamController::class,'getById']);
 Route::post('/exams/create', [ExamController::class,'create']);
 Route::patch('/exams/update/{id}', [ExamController::class,'update']);
 Route::delete('/exams/delete/{id}', [ExamController::class,'delete']);
 
+
 // ROTAS DE EXAM QUESTIONS
 Route::get('/exam-questions/all', [ExamQuestionController::class,'getAll']);
 Route::post('/exam-questions/statement', [ExamQuestionController::class,'getByStatement']);
-Route::get('/exam-questions/{id}', [ExamQuestionController::class,'getById']);
+Route::get('/exam-questions/id/{id}', [ExamQuestionController::class,'getById']);
 Route::post('/exam-questions/create', [ExamQuestionController::class,'create']);
 Route::patch('/exam-questions/update/{id}', [ExamQuestionController::class,'update']);
 Route::delete('/exam-questions/delete/{id}', [ExamQuestionController::class,'delete']);
+
 
 // ROTAS DE EXAM QUESTIONS ALTERNATIVES
 // Route::get('/questions-alternatives/all', [ExamQuestionController::class,'getAll']);
@@ -112,17 +99,19 @@ Route::delete('/exam-questions/delete/{id}', [ExamQuestionController::class,'del
 // Route::patch('/questions-alternatives/update/{id}', [ExamQuestionController::class,'update']);
 // Route::delete('/questions-alternatives/delete/{id}', [ExamQuestionController::class,'delete']);
 
+
 // ROTAS DE EDUCATIONAL LEVELS
 Route::get('/educational-levels/all', [EducationalLevelController::class, 'getAll']);
-Route::get('/educational-levels/{id}', [EducationalLevelController::class, 'getById']);
+Route::get('/educational-levels/id/{id}', [EducationalLevelController::class, 'getById']);
 Route::get('/educational-levels/name', [EducationalLevelController::class, 'getByName']);
 Route::post('/educational-levels/create', [EducationalLevelController::class, 'create']);
 Route::patch('/educational-levels/update/{id}', [EducationalLevelController::class, 'update']);
 Route::delete('/educational-levels/delete/{id}', [EducationalLevelController::class, 'delete']);
 
+
 // ROTAS DE ACCOUNT PLANS
 Route::get('/account-plans/all', [AccountPlanController::class, 'getAll']);
-Route::get('/account-plans/{id}', [AccountPlanController::class,'getById']);
+Route::get('/account-plans/id/{id}', [AccountPlanController::class,'getById']);
 Route::get('/account-plans/name', [AccountPlanController::class,'getByName']);
 Route::post('/account-plans/create', [AccountPlanController::class,'create']);
 Route::patch('/account-plans/update/{id}', [AccountPlanController::class, 'update']);
