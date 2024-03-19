fe7eb65a6a7f2622f0ee21ce2e6802bca3f4adad
albuquerque-lucas <lucaslpra@gmail.com>
2024-03-19 14:46:05 -0300

    Implementando crud de access_levels

--- EaiPassei-api/app/Http/Controllers/AccessLevelController.php
+++ EaiPassei-api/app/Http/Controllers/AccessLevelController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AccessLevelRequest;
+use App\Services\AccessLevelService;
+use App\Services\DataRetrievalService;
 use Illuminate\Http\Request;
 
 class AccessLevelController extends Controller
 {
-    //
+    protected AccessLevelService $accessLevelService;
+    private DataRetrievalService $dataRetrievalService;
+
+    public function __construct(AccessLevelService $accessLevelService, DataRetrievalService $dataRetrievalService)
+    {
+        $this->accessLevelService = $accessLevelService;
+        $this->dataRetrievalService = $dataRetrievalService;
+    }
+
+    public function getAll(Request $request)
+    {
+        return $this->dataRetrievalService->getAll($this->accessLevelService, $request);
+    }
+
+    public function getById(int $id)
+    {
+        return $this->dataRetrievalService->getById($this->accessLevelService, $id);
+    }
+
+    public function update(Request $request, int $id)
+    {
+        return $this->dataRetrievalService->update($this->accessLevelService, $id, $request);
+    }
+
+    public function delete(int $id)
+    {
+        return $this->dataRetrievalService->delete($this->accessLevelService, $id);
+    }
+
+    public function create(AccessLevelRequest $request)
+    {
+        return $this->dataRetrievalService->create($this->accessLevelService, $request);
+    }
 }
--- EaiPassei-api/app/Models/AccessLevel.php
+++ EaiPassei-api/app/Models/AccessLevel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class AccessLevel extends Model
 {
@@ -25,5 +25,15 @@ class AccessLevel extends Model
     public function accountPlan(): HasMany
     {
         return $this->hasMany(AccountPlan::class);
+    }
+
+    public static function getAllOrdered(string $order, string $orderBy = 'id'): LengthAwarePaginator
+    {
+        return self::orderBy($orderBy, $order)->paginate();
+    }
+
+    public static function getById(int $id): self | null
+    {
+        return self::where('id', $id)->first();
     }
 }
