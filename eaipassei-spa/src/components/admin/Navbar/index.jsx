79b6b655d6aa8839d7530494af00ebb71c04be5c
albuquerque-lucas <lucaslpra@gmail.com>
2024-03-20 15:17:47 -0300

    Criando estado para manipulacao da navbar no componente.

--- eaipassei-spa/src/components/admin/Navbar/index.jsx
+++ eaipassei-spa/src/components/admin/Navbar/index.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './styles/style.css';
 
 export default function AdminNavbar() {
+  const [active, setActive] = useState(false);
+
   return (
     <nav className="admin_navbar">
       <div className="container admin_navbar__container">
-        <button class="admin_navbar__toggler" type="button">
+        <button
+          class="admin_navbar__toggler"
+          type="button"
+          onClick={() => setActive(!active)}
+          >
+
           <FontAwesomeIcon icon={ faBars } className='navbar-bars'/>
         </button>
         <Link to="/" className="navbar-brand">
           EaiPassei
         </Link>
-        <div className="admin_navbar__menu">
+        <div className={`admin_navbar__menu ${active ? 'open' : ''}`}>
           <ul>
             <li className="nav-item">
               <Link to="/" className="nav-link active">
