0cd5ac75d21882f948b9cffa38a0a8746fccf59c
albuquerque-lucas <lucaslpra@gmail.com>
2024-03-26 17:48:07 -0300

    Retirando logs

--- eaipassei-spa/src/layouts/admin.layout.jsx
+++ eaipassei-spa/src/layouts/admin.layout.jsx
@@ -1,11 +1,9 @@
 import ScrollToTop from "react-scroll-to-top";
-import { useState } from "react";
 import AdminNavbar from "../components/admin/Navbar";
 import { useContext, useEffect } from 'react';
 import { Outlet, useLocation, useNavigate, Navigate } from 'react-router-dom';
 import LayoutContext from "../context/Layout/LayoutContext";
 import { AuthContext } from "../context/Authentication/AuthContext";
-import useAuthValidation from "../hooks/useAuthValidation";
 import axios from '../axios';
 import './styles/style.css';
 
@@ -18,44 +16,32 @@ export default function AdminLayout({ children }) {
     setAuthenticated
   } = useContext(AuthContext);
   const { setActive } = useContext(LayoutContext);
-  const [isLoggedIn, setIsLoggedIn] = useState(false); 
   const location = useLocation();
   const navigate = useNavigate();
   
   useEffect(() => {
-    console.log('AdminLayout', location.pathname);
-    console.log('AdminLayout', isLoggedIn);
-    console.log('Foi renderizado!!!!!!');
     setActive(false);
-    let loggedIn = fetchUser();
-    setIsLoggedIn(loggedIn);
+    checkAuthentication();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   
-  const fetchUser = async () => {
-    let loggedIn;
+  const checkAuthentication = async () => {
     try {
       const resp = await axios.get('/admin/user');
       if (resp.status === 200) {
-        console.log('Deu bom');
-        console.log('User', resp.data);
+        console.log('Authenticated : ', authenticated);
         setUser(resp.data);
         setAuthenticated(true);
-        console.log('Authenticated2222', authenticated);
-        loggedIn = true;
       }
     } catch (error) {
       if (error.response && error.response.status === 401) {
         console.log('AuthError', error);
-        console.log('Nao deu bom');
+        console.log('Authenticated : ', authenticated);
         localStorage.removeItem('user');
-        console.log('Authenticated3333', authenticated);
-        loggedIn = false;
+        setAuthenticated(false);
         navigate('/admin/login');
       }
-      setAuthenticated(false);
     }
-    return loggedIn;
   }
 
   return (
