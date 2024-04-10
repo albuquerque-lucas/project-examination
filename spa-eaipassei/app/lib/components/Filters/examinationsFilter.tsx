a50363419e29ac0422949b6193e738dc0753f871
albuquerque-lucas <lucaslpra@gmail.com>
2024-04-10 13:43:48 -0300

    Alterando verbos dos comentarios de addToFilterList para o presente

--- spa-eaipassei/app/lib/components/Filters/examinationsFilter.tsx
+++ spa-eaipassei/app/lib/components/Filters/examinationsFilter.tsx
@@ -35,7 +35,7 @@ export default function ExaminationsFilters() {
       filterValue = textInputRef.current?.value || "";
     }
   
-    // Se filterValue for nulo ou uma string vazia, retorne e não adicione o novo filtro
+    // Se filterValue é nulo ou uma string vazia, retorna e não adiciona o novo filtro
     if (!filterValue) {
       setFilterMessage("O valor do filtro não pode ser vazio");
       return;
@@ -46,20 +46,20 @@ export default function ExaminationsFilters() {
       value: filterValue,
     };
   
-    // Verifique se o filtro já existe
+    // Verifica se o filtro já existe
     const existingFilterIndex = filterList.findIndex(filter => filter.filter === newFilter.filter);
     if (existingFilterIndex !== -1) {
-      // Se o valor do filtro existente for o mesmo, retorne e não adicione o novo filtro
+      // Se o valor do filtro existente é o mesmo, retorna e não adiciona o novo filtro
       if (filterList[existingFilterIndex].value === newFilter.value) {
         setFilterMessage("Este filtro já foi adicionado");
         return;
       } else {
-        // Se o valor do filtro existente for diferente, remova o filtro existente
+        // Se o valor do filtro existente é diferente, remove o filtro existente
         filterList.splice(existingFilterIndex, 1);
       }
     }
   
-    // Verifique se já existem 3 filtros
+    // Verifica se já existem 3 filtros
     if (filterList.length >= 3) {
       setFilterMessage("Não é possível adicionar mais de 3 filtros");
       return;
