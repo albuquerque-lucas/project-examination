da95a8b09ca8f39c304691ef7f49897da191a4da
albuquerque-lucas <lucaslpra@gmail.com>
2024-04-30 16:47:41 -0300

    Alterando efeito de abrir e fechar a navbar. Adicionando estado de mudanca de pagina em AdminLayoutContext.

--- spa-eaipassei/app/admin/layout.tsx
+++ spa-eaipassei/app/admin/layout.tsx
@@ -1,8 +1,17 @@
 'use client';
 
 import AdminLayout from "./AdminLayout";
+import { useEffect, useContext } from "react";
+import { AdminLayoutContext } from "../lib/context/AdminLayoutContext";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { setSidebarOpen, pageChange, setPageChange } = useContext(AdminLayoutContext);
+  useEffect(() => {
+    if (pageChange) {
+      setSidebarOpen(false);
+      setPageChange(false);
+    }
+  }, [pageChange]);
   return (
     <AdminLayout>
         { children }
--- spa-eaipassei/app/lib/components/ListItems/navbarItem.tsx
+++ spa-eaipassei/app/lib/components/ListItems/navbarItem.tsx
@@ -3,13 +3,14 @@ import { AdminLayoutContext } from '@/app/lib/context/AdminLayoutContext';
 import { motion } from 'framer-motion';
 
 export default function NavbarItem({ children }: { children: React.ReactNode }) {
-  const { setSidebarOpen } = useContext(AdminLayoutContext);
+  const { setPageChange } = useContext(AdminLayoutContext);
   return (
     <motion.li
       className="nav-item"
       whileHover={{ scale: 1.015, backgroundColor: 'rgba(255, 255, 255, 0.1)' }}
       transition={{ duration: 0.15 }}
       whileTap={{ backgroundColor: 'transparent'}}
+      onClick={() => setPageChange(true)}
     >
       { children }
     </motion.li>
