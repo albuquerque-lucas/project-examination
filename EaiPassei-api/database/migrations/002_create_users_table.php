9f16e5f2de26c3c634b679482e45ed83925b89e1
albuquerque-lucas <lucaslpra@gmail.com>
2024-05-02 18:10:10 -0300

    Adicionando coluna de imagem de perfil para usuarios no banco de dados.

--- EaiPassei-api/database/migrations/002_create_users_table.php
+++ EaiPassei-api/database/migrations/002_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('profile_img')->nullable();
             $table->string('phone_number')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('username_verified_at')->nullable();
