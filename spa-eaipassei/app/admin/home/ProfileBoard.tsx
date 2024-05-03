293ba567913b86e6c217bf371ca35bf662465ddb
albuquerque-lucas <lucaslpra@gmail.com>
2024-05-03 15:40:38 -0300

    Adicionando estilizacoes para mobile e desktop

--- spa-eaipassei/app/admin/home/ProfileBoard.tsx
+++ spa-eaipassei/app/admin/home/ProfileBoard.tsx
@@ -3,6 +3,7 @@ import { User } from '@/app/lib/types/userTypes';
 import style from '@/app/ui/admin/home/profileBoard.module.css';
 import EditProfileField from './EditProfileField';
 import ImageUploadField from './ImageUploadField';
+import ShowcaseProfileField from './ShowcaseProfileField';
 
 function ProfileBoard({ user }: { user: User}) {
   const firstNameRef = useRef(null);
@@ -10,11 +11,6 @@ function ProfileBoard({ user }: { user: User}) {
   const emailRef = useRef(null);
   const phoneNumberRef = useRef(null);
   const usernameRef = useRef(null);
-  const accountPlanRef = useRef(null);
-  const subscriptionDateRef = useRef(null);
-  const subscriptionDurationDaysRef = useRef(null);
-  const subscriptionFeeRef = useRef(null);
-  const subscriptionMissingDaysRef = useRef(null);
   const imageRef = useRef(null);
 
   return (
@@ -23,11 +19,56 @@ function ProfileBoard({ user }: { user: User}) {
       <div>
         <ImageUploadField label="Imagem" ref={ imageRef } />
         <div className={style.panel_case__edit_profile__data}>
-          <EditProfileField label="Nome" type="text" ref={firstNameRef} defaultValue={user?.first_name || ''} />
-          <EditProfileField label="Sobrenome" type="text" ref={lastNameRef} defaultValue={user?.last_name || ''} />
-          <EditProfileField label="Email" type="email" ref={emailRef} defaultValue={user?.email || ''} />
-          <EditProfileField label="Telefone" type="text" ref={phoneNumberRef} defaultValue={user?.phone_number || ''} />
-          <EditProfileField label="Nome de Usuário" type="text" ref={usernameRef} defaultValue={user?.username || ''} />
+          <EditProfileField
+            label="Nome"
+            type="text" ref={firstNameRef}
+            defaultValue={user?.first_name || ''}
+          />
+          <EditProfileField
+            label="Sobrenome"
+            type="text" ref={lastNameRef}
+            defaultValue={user?.last_name || ''}
+          />
+          <EditProfileField
+            label="Email"
+            type="email" ref={emailRef}
+            defaultValue={user?.email || ''}
+          />
+          <EditProfileField
+            label="Telefone"
+            type="text" ref={phoneNumberRef}
+            defaultValue={user?.phone_number || ''}
+          />
+          <EditProfileField
+            label="Nome de Usuário"
+            type="text" ref={usernameRef}
+            defaultValue={user?.username || ''}
+          />
+          <ShowcaseProfileField
+            label="Nível de Acesso"
+            type="text"
+            defaultValue={user?.account_plan || ''}
+          />
+          <ShowcaseProfileField
+            label="Data de Assinatura"
+            type="text"
+            defaultValue={user?.subscription_date || ''}
+          />
+          <ShowcaseProfileField
+            label="Duração da Assinatura"
+            type="text"
+            defaultValue={user?.subscription_duration_days || ''}
+          />
+          <ShowcaseProfileField
+            label="Taxa de Assinatura"
+            type="text"
+            defaultValue={user?.subscription_fee || ''}
+          />
+          <ShowcaseProfileField
+            label="Dias Restantes"
+            type="text"
+            defaultValue={user?.subscription_missing_days || ''}
+          />
         </div>
       </div>
     </div>
