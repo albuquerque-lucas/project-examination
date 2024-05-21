88357f854429540e84c2b85f16ad66cec34f2d46
albuquerque-lucas <lucaslpra@gmail.com>
2024-05-21 15:28:15 -0300

    Alterando resources de Exam

---
+++ EaiPassei-api/app/Http/Resources/ExamMinResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ExamMinResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'examination' => $this->examination->title ?? "Não informado",
+            'description'=> $this->description,
+            'date' => $this->date,
+            'subjects' => SubjectMinResource::collection($this->subjects),
+            'questions_count' => count($this->examQuestions),
+        ];
+    }
+}
