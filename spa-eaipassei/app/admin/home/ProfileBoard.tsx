2869271412513268f2db149077b9bcd0f573e835
albuquerque-lucas <lucaslpra@gmail.com>
2024-05-23 17:07:21 -0300

    Realizando alteracoes no estilo da home page.

--- spa-eaipassei/app/admin/home/ProfileBoard.tsx
+++ spa-eaipassei/app/admin/home/ProfileBoard.tsx
@@ -36,73 +36,46 @@ function ProfileBoard({ user }: { user: User}) {
           ref={ imageRef }
           userId={ user.id }
         />
-        <div>
-          <EditProfileField
-            label="Nome:"
-            type="text"
-            ref={firstNameRef}
-            defaultValue={user?.first_name || ''}
-            userId={ user.id }
-            field="first_name"
-          />
-          <EditProfileField
-            label="Sobrenome:"
-            type="text"
-            ref={lastNameRef}
-            defaultValue={user?.last_name || ''}
-            userId={ user.id }
-            field="last_name"
-          />
-          <EditProfileField
-            label="Email:"
-            type="email"
-            ref={emailRef}
-            defaultValue={user?.email || ''}
-            userId={ user.id }
-            field="email"
-          />
-          <EditProfileField
-            label="Telefone:"
-            type="text"
-            ref={phoneNumberRef}
-            defaultValue={user?.phone_number || ''}
-            userId={ user.id }
-            field="phone_number"
-          />
-          <EditProfileField
-            label="Username:"
-            type="text"
-            ref={usernameRef}
-            defaultValue={user?.username || ''}
-            userId={ user.id }
-            field="username"
-          />
-          {/* <ShowcaseProfileField
-            label="Nível de Acesso"
-            type="text"
-            defaultValue={user?.account_plan || ''}
-          />
-          <ShowcaseProfileField
-            label="Data de Assinatura"
-            type="text"
-            defaultValue={user?.subscription_date || ''}
-          />
-          <ShowcaseProfileField
-            label="Duração da Assinatura"
-            type="text"
-            defaultValue={user?.subscription_duration_days || ''}
-          />
-          <ShowcaseProfileField
-            label="Taxa de Assinatura"
-            type="text"
-            defaultValue={user?.subscription_fee || ''}
-          />
-          <ShowcaseProfileField
-            label="Dias Restantes"
-            type="text"
-            defaultValue={user?.subscription_missing_days || ''}
-          /> */}
-        </div>
+        <EditProfileField
+          label="Nome:"
+          type="text"
+          ref={firstNameRef}
+          defaultValue={user?.first_name || ''}
+          userId={ user.id }
+          field="first_name"
+        />
+        <EditProfileField
+          label="Sobrenome:"
+          type="text"
+          ref={lastNameRef}
+          defaultValue={user?.last_name || ''}
+          userId={ user.id }
+          field="last_name"
+        />
+        <EditProfileField
+          label="Email:"
+          type="email"
+          ref={emailRef}
+          defaultValue={user?.email || ''}
+          userId={ user.id }
+          field="email"
+        />
+        <EditProfileField
+          label="Telefone:"
+          type="text"
+          ref={phoneNumberRef}
+          defaultValue={user?.phone_number || ''}
+          userId={ user.id }
+          field="phone_number"
+        />
+        <EditProfileField
+          label="Username:"
+          type="text"
+          ref={usernameRef}
+          defaultValue={user?.username || ''}
+          userId={ user.id }
+          field="username"
+        />
       </div>
     </motion.div>
   )
