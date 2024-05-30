e2350e6c2c15fdbb8d199b888f862f11c98f95e5
albuquerque-lucas <lucaslpra@gmail.com>
2024-05-30 19:39:54 -0300

    Incrementando estilos e funcionalidades para a pagina examination/id. Criando o frontend para alteracao de dados da prova.

--- spa-eaipassei/app/admin/manage/examinations/[id]/EntityInfoBoard.tsx
+++ spa-eaipassei/app/admin/manage/examinations/[id]/EntityInfoBoard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { ExamsContext } from '@/app/lib/context/ExamsContext';
 import { Exam } from '@/app/lib/types/examTypes';
@@ -8,6 +8,8 @@ import { IoCloseSharp } from "react-icons/io5";
 import { useGetExamById } from '@/app/lib/hooks/useGetExamById';
 import { formatDate } from '@/app/lib/utils/formatDate';
 import { motion } from 'framer-motion';
+import { MdCancelPresentation } from "react-icons/md";
+import { IoCheckbox } from "react-icons/io5";
 import style from '@/app/ui/admin/cards/entityInfoBoard.module.css';
 
 interface EntityInfoBoardProps {
@@ -15,15 +17,18 @@ interface EntityInfoBoardProps {
 }
 
 export default function EntityInfoBoard({ exam }: EntityInfoBoardProps) {
+  const [editMode, setEditMode] = useState({
+    date: false,
+    description: false,
+  });
+
+  const dateRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLInputElement>(null);
+
+  const tapScale = 0.997;
   const router = useRouter();
   const { id, date, description, questions_count, subjects } = exam;
   const formattedDate = date && formatDate(date);
-  const showCase = {
-    "Id": id,
-    "Data": formattedDate,
-    "Descrição": description,
-    "Questões": questions_count,
-  };
 
   const { setEntity } = useGetExamById();
 
@@ -32,54 +37,134 @@ export default function EntityInfoBoard({ exam }: EntityInfoBoardProps) {
     setEntityToDelete,
   } = useContext(ExamsContext);
 
+  const handleEditClick = (field: 'date' | 'description', event: React.MouseEvent) => {
+    const inputRef = field === 'date' ? dateRef.current : descriptionRef.current;
+    if (event.target !== inputRef) {
+      setEditMode(prevState => ({
+        ...prevState,
+        [field]: !prevState[field]
+      }));
+    }
+  };
+
+  const handleCancelClick = (field: 'date' | 'description', event: React.MouseEvent) => {
+    event.stopPropagation();
+    const inputRef = field === 'date' ? dateRef.current : descriptionRef.current;
+    if (inputRef && inputRef.value !== '') {
+      inputRef.value = '';
+    } else {
+      setEditMode(prevState => ({
+        ...prevState,
+        [field]: false
+      }));
+    }
+  };
+
+  const handleSaveClick = (field: 'date' | 'description', event: React.MouseEvent) => {
+    event.stopPropagation();
+    // Implement saving logic here if needed
+    setEditMode(prevState => ({
+      ...prevState,
+      [field]: false
+    }));
+  };
+
   return (
-    <div className={ style.exam_details } >
+    <div className={style.exam_details}>
       <motion.button
-        className={ style.details_close__btn }
+        className={style.details_close__btn}
         whileTap={{ scale: 0.9 }}
-        onClick={ () => setEntity(null) }
+        onClick={() => setEntity(null)}
       >
         <IoCloseSharp />
       </motion.button>
-      {
-        Object.entries(showCase).map(([key, value]) => (
-          <p key={key}>
-            <span className={ style.detail_key }>{key}: </span>
-            <span className={ style.detail_value }>{value}</span>
-          </p>
-        ))
-      }
-      <p>
-        <span className={ style.detail_key }>Matérias: </span>
-        <span className={ style.detail_value }>
-          {
-            subjects &&
-            subjects
-              .map(subject => subject.title)
-              .join(', ')
-          }
+      <motion.p whileTap={{ scale: tapScale }} onClick={(event) => handleEditClick('date', event)}>
+        <span className={style.detail_key}>Data: </span>
+        {editMode.date ? (
+          <span className={style.detail_input_span}>
+            <input
+              type="text"
+              ref={dateRef}
+              defaultValue=''
+              placeholder={formattedDate ? formattedDate : 'Digite uma data...'}
+            />
+            <motion.button 
+              className={style.edit_exam__btn} 
+              whileTap={{ scale: 0.9 }} 
+              onClick={(event) => handleSaveClick('date', event)}
+            >
+              <IoCheckbox />
+            </motion.button>
+            <motion.button 
+              className={style.cancel_edit__btn} 
+              whileTap={{ scale: 0.9 }} 
+              onClick={(event) => handleCancelClick('date', event)}
+            >
+              <MdCancelPresentation />
+            </motion.button>
+          </span>
+        ) : (
+          <span className={style.detail_value}>{formattedDate}</span>
+        )}
+      </motion.p>
+      <motion.p whileTap={{ scale: tapScale }} onClick={(event) => handleEditClick('description', event)}>
+        <span className={style.detail_key}>Descrição: </span>
+        {editMode.description ? (
+          <span className={style.detail_input_span}>
+            <input
+              type="text"
+              ref={descriptionRef}
+              defaultValue=''
+              placeholder={description ? `${description.substring(0, 7)}...` : 'Digite uma descrição...'}
+            />
+            <motion.button 
+              className={style.edit_exam__btn} 
+              whileTap={{ scale: 0.9 }} 
+              onClick={(event) => handleSaveClick('description', event)}
+            >
+              <IoCheckbox />
+            </motion.button>
+            <motion.button 
+              className={style.cancel_edit__btn} 
+              whileTap={{ scale: 0.9 }} 
+              onClick={(event) => handleCancelClick('description', event)}
+            >
+              <MdCancelPresentation />
+            </motion.button>
+          </span>
+        ) : (
+          <span className={style.detail_value}>{description}</span>
+        )}
+      </motion.p>
+      <motion.p whileTap={{ scale: tapScale }}>
+        <span className={style.detail_key}>Questões: </span>
+        <span className={style.detail_value}>{questions_count}</span>
+      </motion.p>
+      <motion.p whileTap={{ scale: tapScale }}>
+        <span className={style.detail_key}>Matérias: </span>
+        <span className={style.detail_value}>
+          {subjects && subjects.map(subject => subject.title).join(', ')}
         </span>
-      </p>
-      <div className={ style.see_exam__container }>
-        <motion.button 
-          className={ style.see_exam__btn }
-          whileTap={ { scale: 0.99 } }
-          onClick={ () => router.push(`/admin/manage/examinations/${id}/questions`) }
+      </motion.p>
+      <div className={style.see_exam__container}>
+        <motion.button
+          className={style.see_exam__btn}
+          whileTap={{ scale: 0.99 }}
+          onClick={() => router.push(`/admin/manage/examinations/${id}/questions`)}
         >
           Ver Prova
         </motion.button>
-        <motion.button 
-          className={ style.delete_exam__btn }
-          whileTap={ { scale: 0.99 } }
-          onClick={ () => {
+        <motion.button
+          className={style.delete_exam__btn}
+          whileTap={{ scale: 0.99 }}
+          onClick={() => {
             setEntityToDelete(exam.id);
             setDeletionMode(true);
-          
-          } }
+          }}
         >
           Deletar
         </motion.button>
       </div>
     </div>
-  )
+  );
 }
