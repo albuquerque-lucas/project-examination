5b7993ea6f93f500cf4b714efb455be79b115c35
albuquerque-lucas <lucaslpra@gmail.com>
2024-06-09 10:52:03 -0300

    Retirando logs de layout.tsx

--- spa-eaipassei/app/admin/layout.tsx
+++ spa-eaipassei/app/admin/layout.tsx
@@ -1,10 +1,6 @@
 import AdminLayout from "./AdminLayout";
-import { getServerSession } from "next-auth";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession();
-  console.log('Log do lado do servidor');
-  console.log('Log de sessao', session);
   return (
     <AdminLayout>
       { children }
